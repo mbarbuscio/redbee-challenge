f14eb90b3328b994dcb826b0607b633415c30b61
mbarbuscio <mbarbuscio@live.com>
2019-02-16 19:53:46 -0300

    added user

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -2,6 +2,8 @@ CREATE SCHEMA redbee;
 
 USE redbee;
 
+GRANT ALL PRIVILEGES ON *.* TO 'api.user'@'localhost' IDENTIFIED BY 'Pa$$w0rD';
+
 CREATE TABLE `Board` (
   `Id` bigint(20) NOT NULL AUTO_INCREMENT,
   `BoardNm` varchar(255) NOT NULL,
